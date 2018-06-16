55c0fcd559cd1d434daf1e89f78409c2729f1797
barashe <eranbarash@gmail.com>
2018-06-16 13:58:42 +0300

    Feature extraction methods are now available.

--- bacpacs.py
+++ bacpacs.py
@@ -1,16 +1,23 @@
 import glob
-
+from os import mkdir
 from os.path import join
 from Bio import SeqIO
 
-from cdhit import run_cdhit
+from util import cdhit, cdhit_2d, orgs_to_vecs
+
 
 class Bacpacs(object):
     def __init__(self, output_dir):
         self._output_dir = output_dir
 
     def merge_genome_files(self, genomes_dir, output_path=None):
-        """Merges all the raw training faa files.
+        """Merges the raw training faa files.
+
+        Parameters
+        ----------
+        genomes_dir
+        output_path
+
         """
         if output_path is None:
             output_path = join(self._output_dir, 'merged.faa')
@@ -27,6 +34,16 @@ class Bacpacs(object):
 
     def reduce(self, long_percent=10, merged_path=None, output_path=None):
         """Samples the longest 10% proteins from the merged fasta file
+
+        Parameters
+        ----------
+        long_percent : float, optional
+            Determines the percentage of long proteins to be used for creating protein families
+        merged_path : basestring, optional
+            Path to merged faa file. If None, the path used by self.merge_genomes_files is used.
+        output_path : basestring, optional
+            Output path. If None, saves 'reduced.faa' in the initial output dir (created in self.merge()).
+
         """
         if output_path is None:
             output_path = join(self._output_dir, 'reduced.faa')
@@ -46,6 +63,22 @@ class Bacpacs(object):
         self.reduced_path_ = output_path
 
     def cluster(self, memory=800, n_jobs=1, cdhit_path='.', reduced_path=None, output_path=None):
+        """
+
+        Parameters
+        ----------
+        memory : int, optional
+            Memory limit (in MB) for CD-HIT, 0 for unlimited.
+        n_jobs : int, optional
+            Number of threads for CD-HIT. 0 to use all CPUs.
+        cdhit_path : basestring, optional
+            Path to CD-HIT
+        reduced_path : basestring, optional
+            Path to reduced faa file. If None, the path used by self.reduced_path_ is used (created in self.reduce()).
+        output_path : basestring, optional
+            Output path. If None, 'protein_families' is saved in self.output_dir).
+
+        """
         if output_path is None:
             output_path = join(self._output_dir, 'protein_families')
         if reduced_path is None:
@@ -53,6 +86,73 @@ class Bacpacs(object):
                 raise ValueError('No input fasta file')
             reduced_path = self.reduced_path_
         print 'Clustering genomes.'
-        run_cdhit(reduced_path, output_path, memory, n_jobs, cdhit_path)
-        print 'Clustering finished successfully. Protein families dump in {}'.format(output_path)
+        cdhit(reduced_path, output_path, memory, n_jobs, cdhit_path)
+        print 'Clustering finished successfully. Protein families dumped in {}'.format(output_path)
         self.pf_path_ = output_path
+
+    def extract_features(self, genomes_dir, feats_type='pred', memory=800, n_jobs=1, cdhit_path='.',
+                         pf_path=None, output_clusters_dir=None, output_features_path=None):
+        """Creates feature vectors for training/predicting genomes. Runs CD-HIT-2D for every genome, against the
+        previously created protein families.
+
+        Parameters
+        ----------
+        genomes_dir : basestring
+            Path to a directory containing genome .faa files.
+        feats_type : ['train', 'pred']
+            Indication whether genomes are used for training, or for prediction.
+        memory : int, optional
+            Memory limit (in MB) for CD-HIT-2D, 0 for unlimited.
+        n_jobs : int, optional
+            Number of threads for CD-HIT-2D. 0 to use all CPUs.
+        cdhit_path : basestring, optional
+            Path to CD-HIT
+        pf_path : basestring, optional
+            Path to protein families file. If None, the path used by self.pf_path_ is used (created in self.cluster()).
+        output_clusters_dir : basestring, optional
+            Output dir for resulting genome clusters. If None, creates 'train_clusters'/'pred_clusters'
+            (depending on 'feats_type') in self.output_dir.
+        output_features_path: basestring, optional
+            Output path for the resulting features. If None, 'pred_feats.pkl'/'train_feats.pkl' (depending on
+            'feats_type') is saved in self.output_dir.
+
+        """
+        if feats_type == 'pred':
+            training = False
+        elif feats_type == 'train':
+            training = True
+        else:
+            raise ValueError("'{}' is not a valid feats_type. Use 'pred' or 'train'")
+        if output_clusters_dir is None:
+            dir_name = 'train_clusters' if training else 'pred_clusters'
+            output_clusters_dir = join(self._output_dir, dir_name)
+        try:
+            mkdir(output_clusters_dir)
+        except Exception:
+            raise IOError('Output dir {} already exists.'.format(output_clusters_dir))
+        if output_features_path is None:
+            features_file = 'train_feats.pkl' if training else 'pred_feats.pkl'
+            output_features_path = join(self._output_dir, features_file)
+        if pf_path is None:
+            if not hasattr(self, 'pf_path_'):
+                raise ValueError("Please specify a valid 'pf_path' or run self.cluster()")
+            pf_path = self.pf_path_
+        if training:
+            feat_list = [rec.id for rec in SeqIO.parse(pf_path, 'fasta')]
+            self.feat_list_ = feat_list
+        else:
+            if not hasattr(self, 'features_'):
+                raise ValueError("Training features were not yet created. Run with 'feats_type='train''")
+        feat_list = self.feat_list_
+        print 'Running genomes against protein families representatives'
+        for genome in glob.glob(join(genomes_dir, '*.faa')):
+            cdhit_2d(genome, pf_path, output_clusters_dir, memory, n_jobs, cdhit_path)
+        print 'Organizing features'
+        orgs_to_vecs(feat_list, output_clusters_dir, output_features_path)
+        print 'Done. Features dumped to {}'.format(output_features_path)
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
--- util.py
+++ util.py
@@ -1,9 +1,17 @@
 import subprocess
+import glob
+import numpy as np
+import pandas as pd
 
 from os import unlink
 from os.path import basename, splitext, join
 
-def run_cdhit(input_path, output_path, m, t, cdhit_path):
+
+def get_file_name(path):
+    return splitext(basename(path))[0]
+
+
+def cdhit(input_path, output_path, m, t, cdhit_path):
     """A wrapper for CD-HIT. Runs CD-HIT greedy mode.
 
     Parameters
@@ -13,7 +21,7 @@ def run_cdhit(input_path, output_path, m, t, cdhit_path):
     output_path : basestring
         Output path.
     m : int
-        Memory limit (in MB) for the CD-HIT, 0 for unlimited.
+        Memory limit (in MB) for CD-HIT, 0 for unlimited.
     t : int
         Number of threads. 0 to use all CPUs.
     cdhit_path : basestring
@@ -37,17 +45,36 @@ def cdhit_2d(org_path, clusters_path, output_dir, m, t, cdhit_path):
     output_dir : basestring
         Path to output directory. Resulting file will have a .clstr extension.
     m : int
-        Memory limit (in MB) for the CD-HIT, 0 for unlimited.
+        Memory limit (in MB) for CD-HIT-2D, 0 for unlimited.
     t : int
         Number of threads. 0 to use all CPUs.
     cdhit_path : basestring
         Path to CD-HIT.
 
     """
-    org_name = splitext(basename(org_path))[0]
+    org_name = get_file_name(org_path)
     output_path = join(output_dir, org_name)
     cmd_line = '{}cd-hit-2d -i {} -i2 {} -o {} -c 0.4 -n 2 -d 0 -M {} -T {} -g 1'
     cmd_line = cmd_line.format(cdhit_path + '/', clusters_path, org_path, output_path, m, t)
     subprocess.check_call(cmd_line.split())
     # We are only interested in output_path.clstr, which is automatically created by CD-HIT-2D
     unlink(output_path)
+
+
+def orgs_to_vecs(feat_list, clusters_dir, output_path):
+    # returns a train/test set in the following format
+    # [[org_ids][org_feats][org_pathogenicities]]
+    cluster_files = glob.glob(join(clusters_dir, '*.clstr'))
+    genome_ids = [get_file_name(cluster_file) for cluster_file in cluster_files]
+    ids_and_clusters = pd.Series(cluster_files, genome_ids)
+    features = pd.DataFrame(0, index=genome_ids, columns=feat_list, dtype=np.bool)
+    for genome_id, feat_vec in features.iterrows():
+        genome_cluster_file = ids_and_clusters[genome_id]
+        tmp_feat = ''
+        with open(genome_cluster_file) as f:
+            for line in f:
+                if line[0] == '0':
+                    tmp_feat = line.split()[2][1:-3]
+                elif line[0] == '1':
+                    feat_vec.loc[tmp_feat] = True
+    features.to_pickle(output_path)
