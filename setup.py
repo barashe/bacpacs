fd06a14dd0980a089d68aec92337f62dca0d9f5a
barashe <eranbarash@gmail.com>
2018-06-17 12:32:16 +0300

    setup + __init

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bacpacs",
+    version="0.0.1",
+    author="Eran Barash",
+    author_email="barashe@post.bgu.ac.il",
+    description="Bacterial Pathogenicity Classification via Sparse-SVM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/barashe/bacpacs",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ),
+)
